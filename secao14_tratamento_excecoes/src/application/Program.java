4fbc445d1365928dce13b07ebe34f7562a9687b6
Maurício Citadini Biléssimo <mauricioc.bilessimo@gamil.com>
2025-05-08 15:27:03 -0300

    Implementadod a versão final de tratamento de exceções (Boa)

--- secao14_tratamento_excecoes/src/application/Program.java
+++ secao14_tratamento_excecoes/src/application/Program.java
@@ -1,33 +1,32 @@
 package application;
 
 import java.text.ParseException;
+import java.util.InputMismatchException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Reservation;
+import model.entities.Reservation;
+import model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 		
-		System.out.print("Número do quanto: ");
-		int number = sc.nextInt();
-		
-		System.out.print("Check-in (dd/MM/yyyy): ");
-		Date checkIn = simpleDateFormat.parse(sc.next());
-		
-		System.out.print("Check-out (dd/MM/yyyy): ");
-		Date checkOut = simpleDateFormat.parse(sc.next());
-		
-		if(!checkOut.after(checkIn)) {
-			System.out.println("Erro na reserva: a data de check-out deve ser posterior à data de check-in");
-		}
-		else {
+		try {
+			System.out.print("Número do quanto: ");
+			int number = sc.nextInt();
+			
+			System.out.print("Check-in (dd/MM/yyyy): ");
+			Date checkIn = simpleDateFormat.parse(sc.next());
+			
+			System.out.print("Check-out (dd/MM/yyyy): ");
+			Date checkOut = simpleDateFormat.parse(sc.next());
+			
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println(reservation);
 			
@@ -39,16 +38,23 @@ public class Program {
 			System.out.print("Check-out (dd/MM/yyyy): ");
 			checkOut = simpleDateFormat.parse(sc.next());
 			
-			String error = reservation.updateDates(checkIn, checkOut);
-			if(error == null) {
-				System.out.println(reservation);
-			}
-			else {
-				System.out.println(error);
-			}			
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println(reservation);
+			
+			sc.close();
 		}
-		
-		sc.close();
+		catch(ParseException e) {
+			System.out.println("ParseException: " + e.getMessage());
+		}
+		catch(DomainException e) {
+			System.out.println("DomainException: " + e.getMessage());
+		}
+		catch(InputMismatchException e) {
+			System.out.println("InputMismatchException: Dados de entrada inválidos: " + e);
+		}
+		catch(RuntimeException e) {
+			System.out.println("Erro inesperado: " + e);
+		}
 	}
 }
 
--- secao14_tratamento_excecoes/src/model/entities/Reservation.java
+++ secao14_tratamento_excecoes/src/model/entities/Reservation.java
@@ -1,8 +1,10 @@
-package entities;
+package model.entities;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
+
+import model.exceptions.DomainException;
 
 public class Reservation {
 	private Integer roomNumber;
@@ -12,7 +14,11 @@ public class Reservation {
 	
 	public Reservation() {}
 
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+		if(!checkOut.after(checkIn)) {
+			throw new DomainException("Erro na reserva: a data de check-out deve ser posterior à data de check-in");
+		}
+		
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -39,20 +45,19 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) throws DomainException {
 		Date now = new Date();
 		
 		if(checkIn.before(now) || checkOut.before(now)) {
-			return "Erro na reserva: as datas de reserva para atualização devem ser datas futuras";
+			throw new DomainException("Erro na reserva: as datas de reserva para atualização devem ser datas futuras");
 		}
 		
 		if(!checkOut.after(checkIn)) {
-			return "Erro na reserva: a data de check-out deve ser posterior à data de check-in";
+			throw new DomainException("Erro na reserva: a data de check-out deve ser posterior à data de check-in");
 		}
 		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
-		return null;
 	}
 	
 	@Override

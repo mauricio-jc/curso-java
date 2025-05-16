2a79af5776f40fdcd5c5b0dbc8fe86d7c4c5764c
Maurício Citadini Biléssimo <mauricioc.bilessimo@gamil.com>
2025-05-16 18:01:26 -0300

    Fazendo o insert

--- demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -25,7 +26,39 @@ public class SellerDaoJDBC implements SellerDao {
 	
 	@Override
 	public void insert(Seller seller) {
+		PreparedStatement st = null;
 		
+		try {
+			st = conn.prepareStatement(
+				"INSERT INTO coursejdbc.seller (name, email, birth_date, base_salary, department_id) VALUES (?, ?, ?, ?, ?)",
+				Statement.RETURN_GENERATED_KEYS
+			);
+			st.setString(1, seller.getName());
+			st.setString(2, seller.getEmail());
+			st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+			st.setDouble(4, seller.getBaseSalary());
+			st.setInt(5, seller.getDepartment().getId());
+			
+			int rows = st.executeUpdate();
+			
+			if(rows > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				
+				if(rs.next()) {
+					int id = rs.getInt(1);
+					seller.setId(id);
+				}
+			}
+			else {
+				throw new DbException("Unesxpected error! No rows affected!");
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override

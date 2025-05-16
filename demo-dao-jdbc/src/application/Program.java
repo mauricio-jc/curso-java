de4ef86ed8e49add89f42d26fec2bbf9dec5218b
Maurício Citadini Biléssimo <mauricioc.bilessimo@gamil.com>
2025-05-16 10:53:49 -0300

    Seller DAO Factory

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		System.out.println(department);
 		
 		Seller seller = new Seller(1, "Fulano", "fulano@email.com", new Date(), 10000.0, department);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	}

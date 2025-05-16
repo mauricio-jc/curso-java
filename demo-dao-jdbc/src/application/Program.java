c51719a78b2a79bbebfa85c9bbcc24f15146179a
Maurício Citadini Biléssimo <mauricioc.bilessimo@gamil.com>
2025-05-16 09:25:41 -0300

    Criado a classe Seller

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,11 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department department = new Department(1, "TI");
 		System.out.println(department);
+		
+		Seller seller = new Seller(1, "Fulano", "fulano@email.com", new Date(), 10000.0, department);
+		
+		System.out.println(seller);
 	}
 }

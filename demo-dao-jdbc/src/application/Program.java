316ae457e44d2949b702f60c57e159b2703c8b7f
Maurício Citadini Biléssimo <mauricioc.bilessimo@gamil.com>
2025-05-16 09:18:21 -0300

    Criado a classe Department

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Department department = new Department(1, "TI");
+		System.out.println(department);
 	}
 }
